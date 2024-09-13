45ac7c4aeed5e46c05b96749b58a10c2b25e5bfd
neeleshsingh843 <neeleshgec843@gmail.com>
2024-09-13 13:33:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,10 @@ resource "aws_instance" "name" {
     ami = "ami-0a4408457f9a03be3"
     instance_type = "t2.micro"
     key_name = "mykp2"
-    count = length(var.test)
+    
     tags = {
-        Name = var.test[count.index]
+        Name = gitsource
     }  
 }
-variable "test" {
-    type = list(string)
-    default = [ "1" , "2" , "3" ]
+
   
-}
